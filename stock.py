99ebbed61933020445f220e1f25646e61891e800
Dean-011 <101196050+Dean-011@users.noreply.github.com>
2022-07-21 13:19:11 -0400

    Finished update
    Stock Work will show entire portfolio numbers and status for the Previous day.

--- stock.py
+++ stock.py
@@ -11,7 +11,7 @@ import json
 WMT_Sh = 1.02
 SPY_Sh = 0.201808
 MSFT_Sh = 0.449768
-BSFD_Sh = 4.28
+BZFD_Sh = 4.28
 TLRY_Sh = 18.62
 TSLA_Sh = 1.16
 META_Sh = float(2.67)
@@ -40,16 +40,17 @@ def meta(yesterday):
         print("Meta price yesterday:")
         print(data.get("Time Series (Daily)").get(yesterday1))
         meta_close_price = data.get("Time Series (Daily)").get(yesterday1).get("4. close")
-        my_meta_price = float(meta_close_price) * META_Sh
-        print("Your total price for META is: "+str(my_meta_price))
-        time.sleep(5)
-        tsla(yesterday)
+        raw_meta_price = float(meta_close_price) * META_Sh
+        print("Your total price for META is: $"+str(raw_meta_price))
+        time.sleep(30)
+        print("Your current portfolio price is: $"+str(raw_meta_price))
+        tsla(yesterday,raw_meta_price)
     else:
         print("Error within the API. Ending proccess")
         exit()
 
 
-def tsla(yesterday):
+def tsla(yesterday, raw_meta_price):
     url = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=TSLA&apikey=UWTP52Z1RRYF3QL9'
     output = requests.get(url)
     data = output.json()
@@ -57,14 +58,19 @@ def tsla(yesterday):
         yesterday1 = str(yesterday)
         print("Tesla price yesterday: ")
         print(data.get("Time Series (Daily)").get(yesterday1))
-        time.sleep(5)
-        msft(yesterday)
+        tsla_close_price = data.get("Time Series (Daily)").get(yesterday1).get("4. close")
+        raw_tsla_price = float(tsla_close_price) * TSLA_Sh
+        print("Your total price for TSLA is: $"+str(raw_tsla_price))
+        time.sleep(30)
+        running_total1 = float(raw_tsla_price) + float(raw_meta_price)
+        print("Your current portfolio price is: $"+str(running_total1))
+        msft(yesterday,running_total1)
     else:
         print("Error within the API. Ending proccess")
         exit()
 
 
-def msft(yesterday):
+def msft(yesterday,running_total1):
     url = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=MSFT&apikey=UWTP52Z1RRYF3QL9'
     output = requests.get(url)
     data = output.json()
@@ -72,14 +78,19 @@ def msft(yesterday):
         yesterday1 = str(yesterday)
         print("Microsoft price yesterday:")
         print(data.get("Time Series (Daily)").get(yesterday1))
-        time.sleep(5)
-        wmt(yesterday)
+        msft_close_price = data.get("Time Series (Daily)").get(yesterday1).get("4. close")
+        raw_msft_price = float(msft_close_price) * MSFT_Sh
+        print("Your total price for MSFT is: $"+str(raw_msft_price))
+        time.sleep(30)
+        running_total2 = float(raw_msft_price) + float(running_total1)
+        print("Your current portfolio price is: $"+str(running_total2))
+        wmt(yesterday,running_total2)
     else:
         print("Error within the API. Ending proccess")
         exit()
 
 
-def wmt(yesterday):
+def wmt(yesterday,running_total2):
     url = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=WMT&apikey=UWTP52Z1RRYF3QL9'
     output = requests.get(url)
     data = output.json()
@@ -87,13 +98,18 @@ def wmt(yesterday):
         yesterday1 = str(yesterday)
         print("Walmart price yesterday: ")
         print(data.get("Time Series (Daily)").get(yesterday1))
-        time.sleep(5)
-        spy(yesterday)
+        wmt_close_price = data.get("Time Series (Daily)").get(yesterday1).get("4. close")
+        raw_wmt_price = float(wmt_close_price) * WMT_Sh
+        print("Your total price for WMT is: $"+str(raw_wmt_price))
+        time.sleep(30)
+        running_total3 = float(raw_wmt_price) + float(running_total2)
+        print("Your current portfolio price is: $"+str(running_total3))
+        spy(yesterday,running_total3)
     else:
         print("Error within the API. Ending proccess")
 
 
-def spy(yesterday):
+def spy(yesterday,running_total3):
     url = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=SPY&apikey=UWTP52Z1RRYF3QL9'
     output = requests.get(url)
     data = output.json()
@@ -101,14 +117,19 @@ def spy(yesterday):
         yesterday1 = str(yesterday)
         print("S&P500 price yesterday: ")
         print(data.get("Time Series (Daily)").get(yesterday1))
-        time.sleep(5)
-        tlry(yesterday)
+        spy_close_price = data.get("Time Series (Daily)").get(yesterday1).get("4. close")
+        raw_spy_price = float(spy_close_price) * SPY_Sh
+        print("Your total price for SPY is: $"+str(raw_spy_price))
+        time.sleep(30)
+        running_total4 = float(raw_spy_price) + float(running_total3)
+        print("Your current portfolio price is: $"+str(running_total4))
+        tlry(yesterday,running_total4)
     else:
         print("Error within the API. Ending proccess")
         exit()
 
 
-def tlry(yesterday):
+def tlry(yesterday,running_total4):
     url = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=TLRY&apikey=UWTP52Z1RRYF3QL9'
     output = requests.get(url)
     data = output.json()
@@ -116,14 +137,19 @@ def tlry(yesterday):
         yesterday1 = str(yesterday)
         print("Tilray Brands price yesterday: ")
         print(data.get("Time Series (Daily)").get(yesterday1))
-        time.sleep(5)
-        bzfd(yesterday)
+        tlry_close_price = data.get("Time Series (Daily)").get(yesterday1).get("4. close")
+        raw_tlry_price = float(tlry_close_price) * TLRY_Sh
+        print("Your total price for TLRY is: $"+str(raw_tlry_price))
+        time.sleep(30)
+        running_total5 = float(raw_tlry_price) + float(running_total4)
+        print("Your current portfolio price is: $"+str(running_total5))
+        bzfd(yesterday,running_total5)
     else:
         print("Error within the API. Ending proccess")
         exit()
 
 
-def bzfd(yesterday):
+def bzfd(yesterday,running_total5):
     url = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=BZFD&apikey=UWTP52Z1RRYF3QL9'
     output = requests.get(url)
     data = output.json()
@@ -131,24 +157,14 @@ def bzfd(yesterday):
         yesterday1 = str(yesterday)
         print("BuzzFeed price yesterday: ")
         print(data.get("Time Series (Daily)").get(yesterday1))
-        time.sleep(5)
-        calculate()
+        bzfd_close_price = data.get("Time Series (Daily)").get(yesterday1).get("4. close")
+        raw_bzfd_price = float(bzfd_close_price) * BZFD_Sh
+        print("Your total price for BZFD is: $"+str(raw_bzfd_price))
+        time.sleep(30)
+        running_total6 = float(raw_bzfd_price) + float(running_total5)
+        print("Your total portfolio price is: $"+str(running_total6))
     else:
         print("Error within the API. Ending proccess")
         exit()
-
-
-
-
-
-def calculate(my_meta_price):
-    print("Your total stock portfolio is : $"+my_meta_price)
-    #total_portfolio = my_meta_price
-    return
-
-
-
-
-
-       
+     
 main() 
